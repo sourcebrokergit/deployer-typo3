7f051b1bfd4146202909d5c93f323d1d4da6e8b1
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2025-03-29 19:56:50 +0100

    Add overwrite for bin/php. Fix path for overwrite of bin/composer.

--- config/loader.php
+++ config/loader.php
@@ -2,7 +2,10 @@
 
 return [
     [
-        'path' => 'vendor/sourcebroker/deployer-extended/includes/composer.php',
+        'path' => 'vendor/sourcebroker/deployer-extended/includes/settings/bin_composer.php',
+    ],
+    [
+        'path' => 'vendor/sourcebroker/deployer-extended/includes/settings/bin_php.php',
     ],
     [
         'package' => 'sourcebrokergit/deployer-typo3',
