624f904d3a2b664bbda2dcbc9e06830d26d18958
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2025-03-31 14:22:07 +0200

    Remove old TYPO3 10 compatibility code.

--- includes/typo3/10/task/typo3_cache_flush_pages.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Deployer;
-
-// No cache:flush for pages only in TYPO3 10
-task('typo3:cache:flush:pages', function () {
-    run('cd {{release_or_current_path}} && {{bin/php}} {{bin/typo3}} cache:flush');
-});
--- includes/typo3/10/task/typo3_cache_warmup_system.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Deployer;
-
-// No cache:warmup yet in TYPO3 10
-task('typo3:cache:warmup:system')->disable()->hidden();
